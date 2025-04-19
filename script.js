31d660fe7d1ebc1db15bc53bb12e4a041b7c247b
Gitthismoney22 <jacksonwork469@gmail.com>
2025-04-18 20:14:50 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+document.getElementById("toggle-language").addEventListener("click", function () {
+    alert("Language toggle not implemented yet.");
+});
+function scrollToSection(id) {
+    document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+}
