2eed8bee7222baf1ff35d77fff2ff1b1af9dafa9
Gitthismoney22 <jacksonwork469@gmail.com>
2025-04-19 15:30:39 -0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-document.getElementById("toggle-language").addEventListener("click", function () {
-    alert("Language toggle not implemented yet.");
-});
-function scrollToSection(id) {
-    document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
-}
